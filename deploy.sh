190c15bf01f62b509f6aeae67e13141120db98d1
dilomen <dilomen@outlook.com>
2021-04-05 20:52:48 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 git add .
 git commit -m '更新'
-git push -f https://github.com/Dilomen/Dilomen.github.io.git master
+git push -f https://github.com/Dilomen/Blog.git master
 # 确保脚本抛出遇到的错误
 set -e 
 
